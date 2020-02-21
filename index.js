7f14690a83ae7dbd572831c21fce0648bb3cac0e
ThanhTran <thanhtkh@vsit.com.vn>
2020-02-21 16:37:52 +0700

    Upgrade project can run

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const express = require('express'),
+      port = 8888,
+      http = require ('http');
+    
 const app = express();
-const port = 8888;
 
-app.listen(port, () => console.log('Listening on port: ' + port));
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-} );
+let defaultController = require('./controllers/defaultController')();
+app.use("/default", defaultController);
+
+app.use((req, res, next) => {
+  next(res.status(404).json({
+    data: {},
+    status: 404,
+    message: 'Không tìm thấy trang yêu cầu'
+  }));
+});
+
+http.createServer (app).listen (port);
